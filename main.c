1ca6ddeab27933292700a97a630a955b50c9ee71
Kunanta0 <faxim2020@yandex.ru>
2026-09-22 22:02:48 +0300

    Update main.c

--- main.c
+++ main.c
@@ -11,8 +11,15 @@
 
 const int INDEX_SIZE = 10000;
 
-size_t ReadFile(const char* name, char* buffer, const char* index[], size_t SIZE);
-void PrintFile(const char* index[], size_t nlines, const char* message);
+struct Text
+{
+    const char** index;
+    size_t nlines;
+};
+
+size_t ReadFile(const char* name, char* buffer, size_t SIZE, size_t* length);
+void PrintFile(struct Text data, const char* message);
+void FillIndex(char* buffer, const char* index[], size_t length);
 
 int main(void)
 {
@@ -22,44 +29,53 @@ int main(void)
     size_t SIZE = file_info.st_size;
 
     char buffer[SIZE + 1] = {};
-    const char* index[INDEX_SIZE] = {};
+    size_t length = 0;
+    size_t nlines = ReadFile(filename, buffer, SIZE, &length);
 
-    size_t nlines = ReadFile(filename, buffer, index, SIZE);
-    const char* copy_index[INDEX_SIZE];
+    /*const char** index = (const char**)calloc(nlines, sizeof(char*));
+    const char** copy_index = (const char**)calloc(nlines, sizeof(char*));*/
+
+    const char* index[INDEX_SIZE] = {};
+    const char* copy_index[INDEX_SIZE] = {};
+
+    FillIndex(buffer, index, length);
+
     memcpy(copy_index, index, nlines * sizeof(char*));
 
-    //struct Text data = {index, nlines};
-    //struct Text copy_data = {copy_index, nlines};
+    struct Text data = {index, nlines};
+    struct Text copy_data = {copy_index, nlines};
 
-    qsort(index, nlines, sizeof(char*), CompareStrsStart);
-    PrintFile(index, nlines, "qsort() CompareStrsStart");
+    qsort(data.index, data.nlines, sizeof(char*), CompareStrsStart);
+    PrintFile(data, "qsort() CompareStrsStart");
 
-    BubbleSort(index, nlines, sizeof(char*), CompareStrsEnd);
-    PrintFile(index, nlines, "BubbleSort() CompareStrsEnd");
+    BubbleSort(data.index, data.nlines, sizeof(char*), CompareStrsEnd);
+    PrintFile(data, "BubbleSort() CompareStrsEnd");
 
-    PrintFile(copy_index, nlines, "Original Onegin");
+    PrintFile(copy_data, "Original Onegin");
+
+    //free(index);
+    //free(copy_index);
 
     return 0;
 }
 
-size_t ReadFile(const char* name, char* buffer, const char* index[], size_t SIZE)
+size_t ReadFile(const char* name, char* buffer, size_t SIZE, size_t* length)
 {
     FILE* input_file = fopen(name, "r");
-    size_t length = fread(buffer, sizeof(char), SIZE, input_file);
+    *length = fread(buffer, sizeof(char), SIZE, input_file);
     fclose(input_file);
 
     size_t current_line = 0;
-    index[0] = buffer;
 
-    for (int i = 0; i < length; ++i)
+    for (size_t i = 0; i < *length; ++i)
     {
         if (*(buffer + i) == '\n')
         {
             *(buffer + i) = '\0';
-            if (i + 1 < length)
+            if (i + 1 < *length)
             {
                 if (*(buffer + i + 1) == '\n') ++i;
-                index[++current_line] = buffer + i + 1;
+                ++current_line;
             }
         }
     }
@@ -67,11 +83,26 @@ size_t ReadFile(const char* name, char* buffer, const char* index[], size_t SIZE
     return current_line;
 }
 
-void PrintFile(const char* index[], size_t nlines, const char* message)
+void FillIndex(char* buffer, const char* index[], size_t length)
+{
+    size_t current_line = 0;
+    index[0] = buffer;
+
+    for (size_t i = 0; i < length; ++i)
+    {
+        if ((*(buffer + i) == '\0') && (i + 1 < length))
+        {
+            if (*(buffer + i + 1) == '\n') ++i;
+            index[++current_line] = buffer + i + 1;
+        }
+    }
+}
+
+void PrintFile(struct Text data, const char* message)
 {
     FILE* fo = fopen("output.txt", "a");
     fprintf(fo, "%s\n\n", message);
-    for (size_t i = 0;i < nlines;++i) fprintf(fo, "%s\n", index[i]);
+    for (size_t i = 0;i < data.nlines;++i) fprintf(fo, "%s\n", data.index[i]);
 
     fprintf(fo, "\n************************************************************************************************\n\n");
 
